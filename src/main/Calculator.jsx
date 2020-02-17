bcfe0e828bbe60ad331bb2485bc6fadfac00dc98
Wescley Klein Frohlich <wescley.kf@gmail.com>
2020-02-17 08:20:04 -0300

    First Commit

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+
+
+import Button from '../Components/Button'
+import Display from '../Components/Display'
+
+
+export default class Calculator extends Component {
+
+    constructor(props){
+        super(props)
+        this.cleanMemory = this.cleanMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+
+    cleanMemory(){
+        console.log('limpar')
+    }
+
+    setOperation(operation){
+        console.log(operation)
+    }
+
+    addDigit(n){
+        console.log(n)
+    }
+
+    render (){
+
+        return(
+            <div className="calculator">
+                <Display value={100}/>
+                <Button label="AC" click={this.cleanMemory()} triple />
+                <Button label="/"  click={this.setOperation} operation/>
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" click={this.setOperation} operation/>
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" click={this.setOperation} operation/>
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" click={this.setOperation} operation/>
+                <Button label="0" click={this.addDigit} double />
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOperation} operation/>
+
+            </div>
+        )
+    }
+}
